3c54e67a99be3c59ef2ed6dae07613c6932673b9
mikelagrouw <mikelagrouw@gmail.com>
2016-12-02 20:07:27 +0100

    d3map
    d3map

---
+++ d3map/csv to json d3map.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+json_file = open("file.json", "w")
+dict = {}
+i = 0
+
+with open("csv density.csv", "r") as csvfile:
+    dense = csv.reader(csvfile)
+    print dense
+    for row in dense:
+        dict["land"] =  row[1]
+        dict["density"] = row[6]
+        print dict
+    json = json.dumps(dict)
+    print json
+    #json.file =
+
